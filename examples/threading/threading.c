9d9bf22c30813d97610387085644697892036b92
Silas Sol <silas@DESKTOP-TJLU1U7.lan>
2026-02-13 20:42:09 -0700

    Assignment 4 Part 1; push 20260213 8:41pm; working now

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-// #include <pthread.h>    // ssolusa; is already in threading.h
 
 
 // Optional: use these functions to add debug or error prints to your application
@@ -23,40 +22,44 @@ void* threadfunc(void* thread_param) {
     //       struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 
 
-    struct thread_data* thread_data = (struct thread_data*) thread_param;
-
-
     printf("ssolusa; threadfunc() START...\n");
 
 
+    int rc;
+
+
+    struct thread_data* func_args;
+    func_args = (struct thread_data*) thread_param;
+
+
     printf("ssolusa; sleep ms...\n");
-    usleep(thread_data->wait_to_obtain_ms);    // ssolusa; sleep 1ms before locking
+    usleep(func_args->wait_to_obtain_ms);
 
 
     printf("ssolusa; mutex_lock()...\n");
-    int rc = pthread_mutex_lock(thread_data->mutex);
+    rc = pthread_mutex_lock(func_args->mutex);
     if ( rc != 0 ) {
         printf("ssolusa; ERROR: pthread_mutex_lock() failed; rc = %d...return false...\n", rc);
-        thread_data->thread_complete_success = false;
+        func_args->thread_complete_success = false;
         return thread_param;
     }
 
 
     printf("ssolusa; Sleep ms...\n");
-    usleep(thread_data->wait_to_release_ms);    // ssolusa; wait 1ms after locking.
+    usleep(func_args->wait_to_release_ms);
 
 
     printf("ssolusa; mutex_unlock()...\n");
-    rc = pthread_mutex_unlock(thread_data->mutex);
+    rc = pthread_mutex_unlock(func_args->mutex);
     if ( rc != 0 ) {
         printf("ssolusa; ERROR: pthread_mutex_unlock() failed; rc = %d...return false...\n", rc);
-        thread_data->thread_complete_success = false;
+        func_args->thread_complete_success = false;
         return thread_param;
     }
 
 
     printf("ssolusa; threadfunc() END...rc = %d...returning...return func_args...\n", rc);
-    thread_data->thread_complete_success = true;
+    func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -79,74 +82,44 @@ bool start_thread_obtaining_mutex(
     printf("ssolusa; start_thread_obtaining_mutex() START...\n");
 
 
-    struct thread_data* thread_data = malloc(sizeof(struct thread_data));
-    if ( !thread_data ) {
+    int rc;
+
+
+    struct thread_data* input_data;
+    input_data = (struct thread_data*) malloc(sizeof(struct thread_data));
+    if ( input_data == NULL ) {
         return false;
     }
-    thread_data->mutex = mutex;
-    thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
-    thread_data->wait_to_release_ms = wait_to_release_ms;
-    thread_data->thread_complete_success = false;
+    input_data->mutex = mutex;
+    input_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    input_data->wait_to_release_ms = wait_to_release_ms;
+    input_data->thread_complete_success = false;
 
 
+/*  SOURCE OF ERROR with unit-test.sh
     printf("ssolusa; mutex_init()...\n");
     int rc = pthread_mutex_init(mutex, NULL);
     if ( rc != 0 ) {
         printf("ssolusa; ERROR: pthread_mutex_init() failed; rc = %d...return false...\n", rc);
-        free(thread_data);
+        free(input_data);
         return false;
     }
+*/
 
 
     printf("ssolusa; pthread_create()...\n");
     rc = pthread_create(thread,
                         NULL,
                         threadfunc,
-                        thread_data);    // See threading.h
+                        input_data);    // See threading.h
     if ( rc != 0 ) {
         printf("ssolusa; ERROR: pthread_create() failed; rc = %d...return false...\n", rc);
-        free(thread_data);
+        free(input_data);
         return false;
     }
-
-
-    // printf("ssolusa; sleep 50ms before returning...\n");
-    // usleep(50);
-
-/*
-    printf("ssolusa; try_join()...\n");
-    rc = pthread_tryjoin_np(thread, NULL);
-    if ( rc == EBUSY ) {
-        printf("**********EBUSY***********\n");
-    } else {
-        printf("EBUSY rc = %d\n", rc);
-    }
-*/
-
-    printf("ssolusa; pthread_join()...\n");
-    rc = pthread_join(*thread, NULL);
-    if ( rc != 0 ) {
-        printf("ssolusa; ERROR: pthread_join() failed; rc = %d...return false...\n", rc);
-        free(thread_data);
-        return false;
-    }
-
-
-    pthread_mutex_destroy(mutex);
-    free(thread_data);
-
-
-    // printf("ssolusa; pthread_exit()...\n");     // Don't use; ends ./unit-test.sh early
-    // pthread_exit(thread);
-
-
-    // printf("ssolusa; pthread_detach()...\n");
-    // pthread_detach(*thread);
-    // pthread_mutex_destroy(&mutex->mutex);
 
 
     printf("ssolusa; start_thread_obtaining_mutex() END...rc = %d...returning...\n", rc);
-    // return false;    // Part of original
-    return true;        // Outputs to thread_started; used in TEST_ASSERT_TRUE_MESSAGE(thread_started)
+    return true;    // Was false
 }
 
